bb3405992323b939d4dfdf4d87cf7cf5ae37fce5
Darien Payton <darien.payton71@gmail.com>
2019-06-25 12:40:13 -0500

    configure mysql db with knex

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+require('dotenv').config();
+module.exports = {
+	development: {
+		client: 'mysql',
+		connection: {
+			user: process.env.DB_USER,
+			password: process.env.DB_PW,
+			database: process.env.DB_TEST
+		}
+	}
+
+	// production: {
+	// 	client: 'postgresql',
+	// 	connection: {
+	// 		database: 'my_db',
+	// 		user: 'username',
+	// 		password: 'password'
+	// 	},
+	// 	pool: {
+	// 		min: 2,
+	// 		max: 10
+	// 	},
+	// 	migrations: {
+	// 		tableName: 'knex_migrations'
+	// 	}
+	// }
+};
