e83a230cd84981e0ed0c3c0af918aa96474740fb
Darien Payton <darien.payton71@gmail.com>
2019-06-27 13:40:44 -0500

    created preferences table

---
+++ config/database/migrations/20190627133701_preferences.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('preferences', tbl => {
+		tbl.increments('id');
+		tbl.string('location').notNullable();
+		tbl.string('num_of_roommates').notNullable();
+		tbl.string('prefer_pets').notNullable();
+		tbl.string('private_bathroom').notNullable();
+		tbl.string('transportation_proximity').notNullable();
+		tbl.string('budget').notNullable();
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('preferences');
+};
