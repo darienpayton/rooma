c968335762617c8dfbe47b82608fe0bd477ebb0a
Darien Payton <darien.payton71@gmail.com>
2019-06-27 13:54:44 -0500

    created home table

---
+++ config/database/migrations/20190627134054_home.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('home', tbl => {
+		tbl.increments('id');
+		tbl
+			.integer('user_id')
+			.unsigned()
+			.notNullable();
+		tbl
+			.foreign('user_id')
+			.references('users.id')
+			.onDelete('CASCADE')
+			.onUpdate('CASCASE');
+		tbl.string('address').notNullable();
+		tbl.string('rooms').notNullable();
+		tbl.string('pet_friendly').notNullable();
+		tbl.string('bathroom').notNullable();
+		tbl.string('transportation_proximity').notNullable();
+		tbl.integer('rent').notNullable();
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('home');
+};
