888fa3a21ab7f9e0876418bd92f96d817570b189
Darien Payton <darien.payton71@gmail.com>
2019-11-08 09:29:03 -0600

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    colors: {
+      'ocean-white': {
+        100: '#fefefe',
+        200: '#fcfcfd',
+        300: '#f9fbfc',
+        400: '#f5f7fa'
+      }
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/custom-forms')]
+};
